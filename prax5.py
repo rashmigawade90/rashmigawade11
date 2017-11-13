e93be2c48d784d75cf328c4747c435ad66e11bad
rashmigawade90 <33619029+rashmigawade90@users.noreply.github.com>
2017-11-13 19:28:31 +0530

    python

---
+++ prax5.py
@@ -0,0 +1,8 @@
+import random;
+print(random.random() *200)
+print(random.randint(1,10))
+print(random.randrange(1,10,2))
+print(random.choice( ['red', 'black', 'green'] ))
+myList = [2, 109, False, 10, "Lorem", 482, "Ipsum"]
+print(random.choice(myList))
+
